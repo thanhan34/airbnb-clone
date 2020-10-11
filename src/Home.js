b9b825296b5af26416be0b63bcb297bac583f54b
An Doan <dtan42@gmail.com>
2020-10-12 00:09:11 +1000

    data picker component

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './Banner'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <Banner />
+        </div>
+    )
+}
+
+export default Home
