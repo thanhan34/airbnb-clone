dd3beee48ccc68732049d7cb15d507e2faf540a4
An Doan <dtan42@gmail.com>
2020-10-11 23:12:24 +1000

    header component

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8lKI5QuwA6CFfFsv5kc_Y18jRCkGJkhs",
+    authDomain: "air-bnb-clone-e54bb.firebaseapp.com",
+    databaseURL: "https://air-bnb-clone-e54bb.firebaseio.com",
+    projectId: "air-bnb-clone-e54bb",
+    storageBucket: "air-bnb-clone-e54bb.appspot.com",
+    messagingSenderId: "292106900882",
+    appId: "1:292106900882:web:d987b0557969442f3c5d4b",
+    measurementId: "G-TT4FH5MQL0"
+};
