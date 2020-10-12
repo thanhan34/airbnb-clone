79e90f9653dbe2b077f0f63ea3efd19a24e92725
An Doan <dtan42@gmail.com>
2020-10-12 10:24:41 +1000

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Header';
 import Home from './Home'
 import SearchPage from './SearchPage'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="app">
